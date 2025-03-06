5e03fd3834fb48ed779a225be0156a970c3205cc
CristianaPBastos <cristianabastos.98@gmail.com>
2025-03-06 12:13:40 +0100

    neue Unterseite

--- Start.py
+++ Start.py
@@ -6,10 +6,10 @@ st.title("Meine erste Streamlit App")
 # !! WICHTIG: Eure Emails müssen in der App erscheinen!!
 
 # Streamlit über den Text unten direkt in die App - cool!
-"""
+st.markdown("""
 Diese App wurde von folgenden Personen entwickelt:
 - Cristiana Bastos  (pereicri@students.zhaw.ch)
 - Lou-Salomé Frehner (frehnlou@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
-"""
+""")
