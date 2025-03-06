7bfaa57d7d75b5beaf1e9a5f7c7c4df0cbae7aa5
CristianaPBastos <cristianabastos.98@gmail.com>
2025-03-06 14:13:06 +0100

    :)

--- pages/Blutzucker-Tracker für Diabetiker.py
+++ pages/Blutzucker-Tracker für Diabetiker.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import string
+import random
+
 def startseite():
     st.title("Blutzucker-Tracker für Diabetiker")
     st.write("""
