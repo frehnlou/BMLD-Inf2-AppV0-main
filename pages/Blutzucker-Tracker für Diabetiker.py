da1c6304ff7f485d96eb24729047bcf1c3b504d7
CristianaPBastos <cristianabastos.98@gmail.com>
2025-03-06 12:56:19 +0100

    sorry zeile 87 nochmal geändert :D

--- pages/Blutzucker-Tracker für Diabetiker.py
+++ pages/Blutzucker-Tracker für Diabetiker.py
@@ -84,6 +84,6 @@ wahl = st.sidebar.radio("Gehe zu", ["Startseite", "Passwort-Generator", "Blutzuc
 if wahl == "Startseite":
     startseite()
 elif wahl == "Passwort-Generator":
-    passwort_Generator()
+    passwort_generator()
 elif wahl == "Blutzucker-Tracker":
     blutzucker_tracker()
