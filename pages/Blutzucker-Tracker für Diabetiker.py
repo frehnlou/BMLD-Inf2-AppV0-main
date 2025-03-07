073ded3daa9fb8351c19d366e9f3718e893cf60f
CristianaPBastos <cristianabastos.98@gmail.com>
2025-03-07 15:40:51 +0100

    startseite angepasst

--- pages/Blutzucker-Tracker für Diabetiker.py
+++ pages/Blutzucker-Tracker für Diabetiker.py
@@ -12,10 +12,7 @@ def startseite():
     - Was bringt dir die App?
     - Schnelle Eingabe deines Blutzuckers (mg/dL)
     - Messzeitpunkt wählen (Nüchtern oder nach dem Essen)
-    - Alle Werte speichern & jederzeit abrufen
-    - Tabelle & Diagramm, um deine Trends zu erkennen
-    - Automatische Warnhinweise, wenn dein Blutzucker zu hoch oder zu niedrig ist
-
+    
     Warum diese App?
              
     ✔ Kein lästiges Papier-Tagebuch mehr
