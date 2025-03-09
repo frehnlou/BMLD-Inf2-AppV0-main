7716feb85a43a2f7fcf20339f4e63e762e454b12
CristianaPBastos <cristianabastos.98@gmail.com>
2025-03-09 13:15:24 +0100

    korrektur

--- Start.py
+++ Start.py
@@ -1,20 +1,24 @@
 import streamlit as st
-import pandas as pd
 
-st.title("Blutzucker-Tracker für Diabetiker")
+# Titel in gross und fett
+st.markdown("# Blutzucker-Tracker für Diabetiker")
 
+# Kurze Einleitung
 st.write("""
-Diese App hilft Diabetiker:innen, ihre Blutzuckerwerte einfach zu erfassen, zu speichern und zu analysieren.
-Sie bietet eine intuitive Benutzeroberfläche zur Eingabe der Werte sowie eine Übersicht der bisherigen Messungen.
+Diese App hilft Ihnen, Ihre Blutzuckerwerte einfach zu erfassen, zu speichern und zu analysieren.
 """)
 
-# !! WICHTIG: Eure Emails müssen in der App erscheinen!!
-
-# Streamlit über den Text unten direkt in die App - cool!
+# Infobox mit Hintergrundfarbe
 st.markdown("""
-Diese App wurde von folgenden Personen entwickelt:
-- Cristiana Bastos  (pereicri@students.zhaw.ch)
-- Lou-Salomé Frehner (frehnlou@students.zhaw.ch)
+<blockquote style="background-color:#E8F0FE; padding:10px; border-radius:5px;">
+Diese App hilft Diabetiker:innen, ihre Blutzuckerwerte einfach zu erfassen, zu speichern und zu analysieren.
+Sie bietet eine intuitive Benutzeroberfläche zur Eingabe der Werte sowie eine Übersicht der bisherigen Messungen.
+</blockquote>
+""", unsafe_allow_html=True)
 
-Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
+# Autoren und E-Mails
+st.write("""
+Diese App wurde von **Cristiana Bastos** (<pereicri@students.zhaw.ch>) und  
+**Lou-Salomé Frehner** (<frehnlou@students.zhaw.ch>) im Rahmen des Moduls  
+'BMLD Informatik 2' an der ZHAW entwickelt.
 """)
