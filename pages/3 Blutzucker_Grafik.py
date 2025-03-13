0920440f04ad045dd9b8fdcc56b32e11e6cd3994
CristianaPBastos <cristianabastos.98@gmail.com>
2025-03-13 16:37:27 +0100

    :)

--- pages/3 Blutzucker_Grafik.py
+++ pages/3 Blutzucker_Grafik.py
@@ -8,25 +8,6 @@ set_page_config()
 # Abstand nach oben für bessere Platzierung
 st.markdown("<br>", unsafe_allow_html=True)
 
-# Navigation
-col1, col2, col3, col4 = st.columns(4)
-
-with col1:
-    if st.button("🏠 Startseite"):
-        st.switch_page("app")
-
-with col2:
-    if st.button("🩸 Blutzucker-Tracker"):
-        st.switch_page("pages/Blutzucker_Tracker")
-
-with col3:
-    if st.button("📋 Blutzucker-Werte"):
-        st.switch_page("pages/Blutzucker_Werte")
-
-with col4:
-    if st.button("📊 Blutzucker-Grafik"):
-        st.switch_page("pages/Blutzucker_Grafik")
-
 st.markdown("## 📊 Blutzucker-Grafik")
 
 if 'daten' in st.session_state and st.session_state['daten']:
