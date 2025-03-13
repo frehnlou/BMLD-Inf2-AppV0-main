1ce9516b07214b8b4e10130174efa81514a48d30
CristianaPBastos <cristianabastos.98@gmail.com>
2025-03-13 16:12:04 +0100

    korrektur

--- pages/2 Blutzucker_Werte.py
+++ pages/2 Blutzucker_Werte.py
@@ -5,6 +5,12 @@ def blutzucker_werte():
     if 'daten' in st.session_state and st.session_state['daten']:
         st.markdown("### Gespeicherte Blutzuckerwerte")
         st.table(st.session_state['daten'])
+        
+        # Durchschnitt berechnen
+        durchschnitt = sum(d['blutzuckerwert'] for d in st.session_state['daten']) / len(st.session_state['daten'])
+        
+        # Durchschnittswert anzeigen
+        st.markdown(f"*Durchschnittlicher Blutzuckerwert:* {durchschnitt:.2f} mg/dL")
     else:
         st.warning("Noch keine Daten vorhanden.")
 
