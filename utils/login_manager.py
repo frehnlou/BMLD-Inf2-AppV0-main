75109a020bca7619e4ff823f027b14777849535b
CristianaPBastos <cristianabastos.98@gmail.com>
2025-03-18 23:09:33 +0100

    ...

--- utils/login_manager.py
+++ utils/login_manager.py
@@ -7,18 +7,8 @@ from utils.data_manager import DataManager
 class LoginManager:
     """
     Singleton-Klasse, die den Anwendungszustand, die Speicherung und die Benutzer-Authentifizierung verwaltet.
-    
-    Verwaltet den Zugriff auf das Dateisystem, Benutzeranmeldedaten und den Authentifizierungsstatus
-    mithilfe des Streamlit-Session-States für die Persistenz zwischen App-Neustarts.
-    Bietet Schnittstellen für den Zugriff auf benutzerspezifische und anwendungsweite Datenspeicherung.
     """
     def __new__(cls, *args, **kwargs):
-        """
-        Implementiert das Singleton-Muster, indem die vorhandene Instanz aus dem Session-State zurückgegeben wird.
-
-        Rückgabe:
-            LoginManager: Die Singleton-Instanz, entweder bestehend oder neu erstellt.
-        """
         if 'login_manager' in st.session_state:
             return st.session_state.login_manager
         else:
@@ -29,24 +19,12 @@ class LoginManager:
     def __init__(self, data_manager: DataManager = None,
                  auth_credentials_file: str = 'credentials.yaml',
                  auth_cookie_name: str = 'bmld_inf2_streamlit_app'):
-        """
-        Initialisiert die Komponenten für das Dateisystem und die Authentifizierung, falls noch nicht initialisiert.
-
-        Konfiguriert den Zugriff auf das Dateisystem mit dem angegebenen Protokoll und richtet die Authentifizierung
-        mit Cookie-basierter Sitzungsverwaltung ein.
-
-        Argumente:
-            data_manager: Die DataManager-Instanz für die Datenspeicherung.
-            auth_credentials_file (str): Der Dateiname für die Speicherung der Benutzeranmeldedaten.
-            auth_cookie_name (str): Der Name des Cookies für die Sitzungsverwaltung.
-        """
-        if hasattr(self, 'authenticator'):  # Überprüfen, ob die Instanz bereits initialisiert ist
+        if hasattr(self, 'authenticator'):
             return
         
         if data_manager is None:
             return
 
-        # Initialisiere die Authentifizierung
         self.data_manager = data_manager
         self.auth_credentials_file = auth_credentials_file
         self.auth_cookie_name = auth_cookie_name
@@ -55,33 +33,14 @@ class LoginManager:
         self.authenticator = stauth.Authenticate(self.auth_credentials, self.auth_cookie_name, self.auth_cookie_key)
 
     def _load_auth_credentials(self):
-        """
-        Lädt die Benutzeranmeldedaten aus der konfigurierten Datei.
-
-        Rückgabe:
-            dict: Benutzeranmeldedaten, standardmäßig ein leeres Wörterbuch, falls die Datei nicht gefunden wird.
-        """
         dh = self.data_manager._get_data_handler()
         return dh.load(self.auth_credentials_file, initial_value={"usernames": {}})
 
     def _save_auth_credentials(self):
-        """
-        Speichert die aktuellen Benutzeranmeldedaten in der Datei.
-        """
         dh = self.data_manager._get_data_handler()
         dh.save(self.auth_credentials_file, self.auth_credentials)
 
     def login_register(self, login_title='Anmelden', register_title='Registrieren'):
-        """
-        Zeigt die Authentifizierungsoberfläche an.
-        
-        Zeigt das Anmeldeformular und optional das Registrierungsformular an. Verarbeitet die
-        Authentifizierungs- und Registrierungsabläufe. Stoppt die weitere Ausführung nach der Anzeige.
-
-        Argumente:
-            login_title (str): Titel des Login-Tabs.
-            register_title (str): Titel des Registrierungs-Tabs.
-        """
         if st.session_state.get("authentication_status") is True:
             self.authenticator.logout()
         else:
@@ -92,9 +51,6 @@ class LoginManager:
                 self.register()
 
     def login(self, stop=True):
-        """
-        Zeigt das Anmeldeformular an und verarbeitet den Authentifizierungsstatus.
-        """
         if st.session_state.get("authentication_status") is True:
             self.authenticator.logout()
         else:
@@ -114,12 +70,6 @@ class LoginManager:
                 st.stop()
 
     def register(self, stop=True):
-        """
-        Zeigt das Registrierungsformular an und verarbeitet den Registrierungsablauf.
-        
-        Zeigt Passwortanforderungen an, verarbeitet Registrierungsversuche und speichert
-        Anmeldedaten bei erfolgreicher Registrierung.
-        """
         if st.session_state.get("authentication_status") is True:
             self.authenticator.logout()
         else:
@@ -128,42 +78,94 @@ class LoginManager:
             einen Kleinbuchstaben, eine Ziffer und ein Sonderzeichen aus @$!%*?& enthalten.
             """)
 
-            # Registrierungsformular
+            # Registrierungsformular in einer Tabelle
             st.markdown("### Benutzer registrieren")
-            first_name = st.text_input("Vorname", key="register_first_name")
-            last_name = st.text_input("Nachname", key="register_last_name")
-            email = st.text_input("E-Mail", key="register_email")
-            username = st.text_input("Benutzername", key="register_username")
-            password = st.text_input("Passwort", type="password", key="register_password")
-            repeat_password = st.text_input("Passwort wiederholen", type="password", key="register_repeat_password")
-            password_hint = st.text_input("Passworthinweis (optional)", key="register_password_hint")
-            captcha = st.text_input("Captcha: Bitte geben Sie '1234' ein", key="register_captcha")
+            st.markdown(
+                """
+                <style>
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                }
+                td {
+                    padding: 10px;
+                    vertical-align: top;
+                }
+                </style>
+                """,
+                unsafe_allow_html=True,
+            )
+
+            # Tabelle für das Formular
+            st.markdown(
+                """
+                <table>
+                    <tr>
+                        <td><label for="first_name">Vorname:</label></td>
+                        <td><input type="text" id="first_name" name="first_name" style="width: 100%;"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="last_name">Nachname:</label></td>
+                        <td><input type="text" id="last_name" name="last_name" style="width: 100%;"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="email">E-Mail:</label></td>
+                        <td><input type="email" id="email" name="email" style="width: 100%;"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="username">Benutzername:</label></td>
+                        <td><input type="text" id="username" name="username" style="width: 100%;"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="password">Passwort:</label></td>
+                        <td><input type="password" id="password" name="password" style="width: 100%;"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="repeat_password">Passwort wiederholen:</label></td>
+                        <td><input type="password" id="repeat_password" name="repeat_password" style="width: 100%;"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="password_hint">Passworthinweis:</label></td>
+                        <td><input type="text" id="password_hint" name="password_hint" style="width: 100%;"></td>
+                    </tr>
+                    <tr>
+                        <td><label for="captcha">Captcha (Bitte '1234' eingeben):</label></td>
+                        <td><input type="text" id="captcha" name="captcha" style="width: 100%;"></td>
+                    </tr>
+                </table>
+                """,
+                unsafe_allow_html=True,
+            )
 
             register_button = st.button("Registrieren", key="register_button")
 
             if register_button:
                 # Überprüfen, ob das Captcha korrekt ist
+                captcha = st.session_state.get("captcha", "")
                 if captcha != "1234":
                     st.error("Captcha ist falsch. Bitte versuchen Sie es erneut.")
                     return
 
                 # Überprüfen, ob die Passwörter übereinstimmen
+                password = st.session_state.get("password", "")
+                repeat_password = st.session_state.get("repeat_password", "")
                 if password != repeat_password:
                     st.error("Die Passwörter stimmen nicht überein.")
                     return
 
                 # Überprüfen, ob der Benutzername bereits existiert
+                username = st.session_state.get("username", "")
                 if username in self.auth_credentials["usernames"]:
                     st.error("Benutzername existiert bereits. Bitte wählen Sie einen anderen.")
                     return
 
                 # Benutzer hinzufügen
                 self.auth_credentials["usernames"][username] = {
-                    "first_name": first_name,
-                    "last_name": last_name,
-                    "email": email,
+                    "first_name": st.session_state.get("first_name", ""),
+                    "last_name": st.session_state.get("last_name", ""),
+                    "email": st.session_state.get("email", ""),
                     "password": password,  # Optional: Passwort-Hashing hinzufügen
-                    "password_hint": password_hint
+                    "password_hint": st.session_state.get("password_hint", "")
                 }
 
                 try:
@@ -176,13 +178,6 @@ class LoginManager:
                     st.stop()
 
     def go_to_login(self, login_page_py_file):
-        """
-        Erstellt eine Logout-Schaltfläche, die den Benutzer abmeldet und zur Anmeldeseite weiterleitet.
-        Wenn der Benutzer nicht eingeloggt ist, wird die Anmeldeseite angezeigt.
-
-        Argumente:
-            login_page_py_file (str): Der Pfad zur Python-Datei, die die Anmeldeseite enthält.
-        """
         if st.session_state.get("authentication_status") is not True:
             st.switch_page(login_page_py_file)
         else:
