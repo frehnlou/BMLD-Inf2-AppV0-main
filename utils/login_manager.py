72a5bb7bff8aca0065f470fdd5c859f9a12d63e6
CristianaPBastos <cristianabastos.98@gmail.com>
2025-03-21 14:25:42 +0100

    ..

--- utils/login_manager.py
+++ utils/login_manager.py
@@ -34,11 +34,13 @@ class LoginManager:
         self.auth_cookie_key = secrets.token_urlsafe(32)
         self.auth_credentials = self._load_auth_credentials()
 
-        self.authenticator = stauth.Authenticator(
-            self.auth_credentials['usernames'],
-            self.auth_cookie_name,
-            self.auth_cookie_key
-        )
+        # Debugging-Ausgabe
+        st.write(f"Geladene Anmeldedaten: {self.auth_credentials}")
+
+        self.authenticator = stauth.Authenticate(self.auth_credentials, self.auth_cookie_name, self.auth_cookie_key)
+
+        # Debugging-Ausgabe
+        st.write(f"Authenticator initialisiert: {self.authenticator}")
 
     def _load_auth_credentials(self):
         """
